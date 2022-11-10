236f936684be99f7f7683a7778d02b8d9c87d7e1
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-10 15:19:54 +0100

    Add reference to HTML

---
+++ client/app.js
@@ -0,0 +1,7 @@
+const loginForm = document.getElementById("form");
+// ^^"username"??
+const messagesSection = document.getElementById("message-section");
+const messagesList = document.getElementById("messages-list");
+const addMessageForm = document.getElementById("add-messages-form");
+const userNameInput = document.getElementById("username");
+const messageContentInput = document.getElementById("message-content");
