83e099a84c313e6026deb3ae0a968137cc4643b4
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-10 16:18:54 +0100

    Add login function

--- client/app.js
+++ client/app.js
@@ -6,3 +6,15 @@ const addMessageForm = document.getElementById("add-messages-form");
 const userNameInput = document.getElementById("username");
 const messageContentInput = document.getElementById("message-content");
 const userName = "";
+
+const login = (e) => {
+    e.preventDefault();
+    if (userNameInput.value !== "") {
+        userName = userNameInput.value;
+        loginForm.classList.remove("show");
+        messagesSection.classList.add("show");
+    } else {
+        alert("Error, enter your user name");
+    };
+};
+console.log(userName);
